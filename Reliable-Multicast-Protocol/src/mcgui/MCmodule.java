7dfb11c490b9812bd00a1207bae2ed129222f3bd
Klumpen <david.bennehag@gmail.com>
2015-02-07 15:18:26 +0100

    Added the MultiCast Module which we will primarily be working with. Extends the Multicaster class.

---
+++ Reliable-Multicast-Protocol/src/mcgui/MCmodule.java
@@ -0,0 +1,34 @@
+package mcgui;
+
+public class MCmodule extends Multicaster
+{
+
+	@Override
+	public void init() 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void cast(String messagetext)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void basicreceive(int peer, Message message)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void basicpeerdown(int peer)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+}
